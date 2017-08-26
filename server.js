d233526fde49043617e1df9edd4acefb583722e9
Francisca Echague <franciscaechague@gmail.com>
2017-08-25 22:27:45 -0500

    final push

--- server.js
+++ server.js
@@ -1,120 +1,57 @@
 // Dependencies
-//==============================================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+// Requiring our Note and Article models
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
+// Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-
+// Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
+var PORT = process.env.PORT || 8080;
+
+// Initialize Express
 var app = express();
 
+// Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+// Serve static content
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoScraper");
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper_controller.js");
+
+app.use("/", routes);
+
+mongoose.connect("mongodb://heroku_gnzk5747:4d2121nhgnfbdl1pfirsdepk9n@ds125262.mlab.com:25262/heroku_gnzk5747");
+
 var db = mongoose.connection;
 
+// Show any mongoose errors
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
 });
 
+// Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-
-
-
-
-// Server
-//==============================================================================
-
-//Scrape for the articles
-
-app.get("/scrape", function(req, res) {
-  request("https://www.nytimes.com/", function(error, response, html) {
-    var $ = cheerio.load(html);
-    $("article h2").each(function(i, element) {
-      var result = {};
-
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
-
-      var entry = new Article(result);
-
-      entry.save(function(err, doc) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log(doc);
-        }
-      });
-
-    });
-  });
-  res.send("Scrape Complete");
-});
-
-
-// Grab articles form Mongo DB
-
-app.get("/articles", function(req, res) {
-  Article.find({}, function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-  Article.findOne({ "_id": req.params.id })
-  .populate("note")
-  .exec(function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-app.post("/saved", function(req, res) {
-  
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Listening
-//==============================================================================
-
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on PORT " + PORT);
 });
